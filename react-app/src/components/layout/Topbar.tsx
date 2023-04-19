063b98672e489a2aeda124718e0df08ea24bd092
jonas <jqh@kvalitetsit.dk>
2023-04-19 09:53:45 +0200

    added logout feature

--- react-app/src/components/layout/Topbar.tsx
+++ react-app/src/components/layout/Topbar.tsx
@@ -1,13 +1,13 @@
-import { AppBar, Toolbar, IconButton, Typography, Box, Chip } from "@mui/material";
-import { useContext } from "react";
+import { AppBar, Toolbar, IconButton, Typography, Box, Chip, Avatar, ListItemIcon, MenuItem } from "@mui/material";
+import { useContext, useState } from "react";
 import { theme } from "../../config/theme";
 import MenuIcon from '@mui/icons-material/Menu';
 import { UserContext } from "../../feature/authentication/logic/FetchUser";
-import TagFacesIcon from '@mui/icons-material/TagFaces';
 import keycloak from "../../feature/authentication/Keycloak";
-import { LoginOutlined } from "@mui/icons-material";
+import { LoginOutlined, Logout } from "@mui/icons-material";
 import { Can } from "../../feature/authentication/logic/Can";
 import { Asset, Operation } from "../../feature/authentication/config/ability";
+import Menu from '@mui/material/Menu';
 
 interface TopbarProps { width: number | string, logo?: JSX.Element, mobileOpen?: boolean, setMobileOpen?: (open: boolean) => void, sidebarDisabled?: boolean }
 export function Topbar(props: TopbarProps) {
@@ -19,6 +19,17 @@ export function Topbar(props: TopbarProps) {
 
     const user = useContext(UserContext)
 
+
+
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <AppBar
@@ -55,15 +66,13 @@ export function Topbar(props: TopbarProps) {
                         textDecoration: 'none',
                     }}
                 >
-                    
                     {props.logo}
-                  
                 </Typography>
                 <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
                 <Box sx={{ flexGrow: 0 }}>
                     <Can ability={user?.getAbility()} I={Operation.READ} a={Asset.PRIVATE}>
-                        <IconButton sx={{ p: 0 }}>
-                            <Chip color="secondary" icon={<TagFacesIcon />} /* avatar={<Avatar>{user?.firstName?.charAt(0)}</Avatar>} */ label={user?.firstName} />
+                        <IconButton sx={{ p: 0 }} onClick={handleClick}>
+                            <Avatar sx={{ bgcolor: theme.palette.secondary.main }}>{user?.firstName?.charAt(0)}</Avatar>
                         </IconButton>
                     </Can>
                     <Can ability={user?.getAbility()} I={Operation.READ} a={Asset.PUBLIC}>
@@ -72,9 +81,76 @@ export function Topbar(props: TopbarProps) {
                         </IconButton>
                     </Can>
                 </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    {/*
+                    <MenuItem onClick={() => console.log("Do something!")}>
+                        <Avatar /> Profile
+                    </MenuItem>
+                    <MenuItem onClick={() => console.log("Do something!")}>
+                        <Avatar /> My account
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={() => console.log("Do something!")}>
+                        <ListItemIcon>
+                            <PersonAdd fontSize="small" />
+                        </ListItemIcon>
+                        Add another account
+                    </MenuItem>
+                    <MenuItem onClick={() => console.log("Do something!")}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                */}
+                    <MenuItem onClick={() => keycloak.logout()}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+
 
             </Toolbar>
         </AppBar>
     )
 
 }
+
+
+
