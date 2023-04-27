e375f939ca350d466444f041bc1d210f79ab8e47
jonas <jqh@kvalitetsit.dk>
2023-04-27 13:59:36 +0200

    IMPORTANT - changed the use of mocked api token

--- react-app/src/redux/BaseQuerySettings.tsx
+++ react-app/src/redux/BaseQuerySettings.tsx
@@ -10,8 +10,8 @@ export default function fetchDefaultBaseQuery() {
         mode: "cors",
         baseUrl: baseurl,
         prepareHeaders: (headers, api) => {
-            //const token = keycloak.token
-            const token = "eyJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2ODI1OTUwMTgsImV4cCI6MTY4MjYwNTgxOCwiYXVkIjoiYXVkaWVuY2UiLCJpc3MiOiJpc3N1ZXIiLCJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20ifQ.wMHT4CCZCa49nOdXT6XKKHut8uUk8SxnpLPyHdBaOP8kTDbtVjMWCJwKtgZSRmp64CCtusyhJ4YE76wYYtkwDCV1sL3ocps8j8KAJTgJZqTp-uj8Hzqvw-iqi3Tlfql71gM2u8bBP9LSYEqLLzzOUCUev2oLaxtzze5OAhc2f8SFBlKzM0RFViLrinkl7-JZ-Ngl-qtdd3TQKkHsDNGSUjkVCqEJFl7j5w3tCUchUKY76O-kwcqXAbQhdFxxRRwHqkHeEYPbDddIYM0hP85-TT6uawJGkeWJi3iAkxPzsrugQFSFdPeCZvj55v4lLISnqorFux1JqR26KKWjAIbb_A" // <-- fake token
+            const token = keycloak.token
+            //const token = "eyJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2ODI1OTUwMTgsImV4cCI6MTY4MjYwNTgxOCwiYXVkIjoiYXVkaWVuY2UiLCJpc3MiOiJpc3N1ZXIiLCJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20ifQ.wMHT4CCZCa49nOdXT6XKKHut8uUk8SxnpLPyHdBaOP8kTDbtVjMWCJwKtgZSRmp64CCtusyhJ4YE76wYYtkwDCV1sL3ocps8j8KAJTgJZqTp-uj8Hzqvw-iqi3Tlfql71gM2u8bBP9LSYEqLLzzOUCUev2oLaxtzze5OAhc2f8SFBlKzM0RFViLrinkl7-JZ-Ngl-qtdd3TQKkHsDNGSUjkVCqEJFl7j5w3tCUchUKY76O-kwcqXAbQhdFxxRRwHqkHeEYPbDddIYM0hP85-TT6uawJGkeWJi3iAkxPzsrugQFSFdPeCZvj55v4lLISnqorFux1JqR26KKWjAIbb_A" // <-- fake token
             if (token) {
                 headers.set('Authorization', `Bearer ${token}`);
             }
