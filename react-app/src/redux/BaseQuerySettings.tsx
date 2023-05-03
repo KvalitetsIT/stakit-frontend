a8eed2b67d91640f5b9f2215a63e34d7ee76e64c
jonas <jqh@kvalitetsit.dk>
2023-05-03 15:06:18 +0200

    added status PARTIAL_NOT_OK

--- react-app/src/redux/BaseQuerySettings.tsx
+++ react-app/src/redux/BaseQuerySettings.tsx
@@ -9,8 +9,8 @@ export default function fetchDefaultBaseQuery() {
         mode: "cors",
         baseUrl: baseurl,
         prepareHeaders: (headers, api) => {
-            //const token = keycloak.token
-            const token = "eyJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2ODMxMTYzMzQsImV4cCI6MTY4MzEyNzEzNCwiYXVkIjoiYXVkaWVuY2UiLCJpc3MiOiJpc3N1ZXIiLCJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20ifQ.DqOBXxMLOZpyZLbYOMe5UTRbae8tDEW0brIUc7FmRYo6eam8QxIUQXplq2G3DKbWBdX5HiyUwG-NKPDzwnb1P4qOIMUSVAKJkcy7Ik-2RROVWrL5-267qwL6BBViE7ENCmIyQaabkWYFkoU5q7jVHRADvrqjYgEv9I7dhIfWTgecstlJmFyUjDiVW9vcqrl4v-mFPZhmASOslIJAR9Zn6Ei9sfTuiy53ndWsW2rWYWjK9xBdmyvmW9osza4WlHRy5WQcW64nmxWEGHx73Xo3JJPhhQrP45XdA_TMW75UwoqWf8iohof2l5b1pebYeyYcQA-1SnY1aDtqqd-8-q75JQ" // <-- fake token
+            const token = keycloak.token
+            //const token = "eyJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2ODMxMTYzMzQsImV4cCI6MTY4MzEyNzEzNCwiYXVkIjoiYXVkaWVuY2UiLCJpc3MiOiJpc3N1ZXIiLCJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20ifQ.DqOBXxMLOZpyZLbYOMe5UTRbae8tDEW0brIUc7FmRYo6eam8QxIUQXplq2G3DKbWBdX5HiyUwG-NKPDzwnb1P4qOIMUSVAKJkcy7Ik-2RROVWrL5-267qwL6BBViE7ENCmIyQaabkWYFkoU5q7jVHRADvrqjYgEv9I7dhIfWTgecstlJmFyUjDiVW9vcqrl4v-mFPZhmASOslIJAR9Zn6Ei9sfTuiy53ndWsW2rWYWjK9xBdmyvmW9osza4WlHRy5WQcW64nmxWEGHx73Xo3JJPhhQrP45XdA_TMW75UwoqWf8iohof2l5b1pebYeyYcQA-1SnY1aDtqqd-8-q75JQ" // <-- fake token
             if (token) {
                 headers.set('Authorization', `Bearer ${token}`);
             }
